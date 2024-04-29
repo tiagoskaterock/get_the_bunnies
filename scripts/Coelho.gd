379fe904679b51580610b033cc50ba41b744f31a
tiagoskaterock <tiagolemespalhano@gmail.com>
2024-04-29 02:57:36 -0300

    128. parte 11 - customização da fonte

--- scripts/Coelho.gd
+++ scripts/Coelho.gd
@@ -12,7 +12,7 @@ var rodando
 var direction
 const PRE_CACOS = preload("res://scenes/Cacos.tscn")
 
-func _ready():
+func _ready() -> void:
 	$AnimationPlayer.play("default")
 	randomize()
 	start_x = get_random_start_x()
@@ -23,17 +23,17 @@ func _ready():
 	position.y = start_y
 	position.x = start_x
 
-func _process(delta):
+func _process(delta) -> void:
 	subir_e_descer(delta)
 	ir_para_o_lado(delta)
 	check_max_y()
 	if rodando:
 		rodar(delta)
 	
-func get_random_start_x():
+func get_random_start_x() -> int:
 	return randi() % (int(get_viewport_rect().size.x) - 100) + 50
 	
-func get_random_side_speed():
+func get_random_side_speed() -> float:
 	var metade_da_tela = get_viewport_rect().size.x / 2
 	var x_speed = (randi() % 6) - 3
 	if (start_x < metade_da_tela) and x_speed < 0:
@@ -42,41 +42,45 @@ func get_random_side_speed():
 		x_speed *= -1
 	return x_speed
 	
-func get_random_start_y():
+func get_random_start_y() -> float:
 	var random_number = (randi() % 300) + 50
 	return get_viewport_rect().size.y + random_number
 
-func subir_e_descer(delta):
+func subir_e_descer(delta) -> void:
 	speed -= GRAVITY
 	position.y -= speed * delta
 	encolher()
 	if speed < 0:
 		subindo = false
 		
-func ir_para_o_lado(delta):
+func ir_para_o_lado(delta) -> void:
 	position.x += side_speed
 	
-func encolher():
+func encolher() -> void:
 	scale *= TAXA_DE_ENCOLHIMENTO
 	
-func rodar(delta):
+func rodar(delta) -> void:
 	if direction:
 		rotation += ROTATION_SPEED * delta
 	else:
 		rotation += ROTATION_SPEED * delta * -1
 		
-func check_max_y():
+func check_max_y() -> void:
 	if ! subindo and position.y > start_y:
 		queue_free()
 
-func _on_Button_button_down():	
+func _on_Button_button_down() -> void:
+	add_point()
 	die()
 	
-func die():
+func add_point() -> void:
+	get_parent().add_point()
+	
+func die() -> void:
 	mostra_cacos()
 	queue_free()
 	
-func mostra_cacos():
+func mostra_cacos() -> void:
 	var cacos = PRE_CACOS.instance()
 	get_parent().add_child(cacos)
 	cacos.position = position
--- scripts/Mundo.gd
+++ scripts/Mundo.gd
@@ -1,13 +1,28 @@
 extends Node2D
 
 const PRE_COELHO = preload("res://scenes/Coelho.tscn")
+var points: int = 0
 	
-func _ready():
+func _ready() -> void:
+	update_label_points()
 	$TimerRespawnCoelho.start()
 	
-func _on_TimerRespawnCoelho_timeout():
+func _on_TimerRespawnCoelho_timeout() -> void:
 	var coelho = PRE_COELHO.instance()
 	add_child(coelho)
 	
-func toca_som_de_caco():
+func toca_som_de_caco() -> void:
 	$Caco.play()
+	
+func set_points(new_points) -> void:
+	self.points = new_points
+	
+func get_points() -> int:
+	return self.points
+	
+func add_point() -> void:
+	set_points(get_points() + 1)
+	update_label_points()
+
+func update_label_points() -> void:
+	$LabelPontos.text = str(get_points())
